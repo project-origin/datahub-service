9bcf59349a9dbad358ea23761a9d75a877a277c1
Jakob Kristensen <jakob@jakobk.dk>
2020-04-24 15:53:55 +0200

    Removed default values for ENV vars in settings.py and added one to README

--- src/datahub/settings.py
+++ src/datahub/settings.py
@@ -4,12 +4,11 @@ from datetime import date, datetime, timezone, timedelta
 
 DEBUG = os.environ.get('DEBUG') in ('1', 't', 'true', 'yes')
 
+
 # -- Project -----------------------------------------------------------------
 
 PROJECT_NAME = 'DataHub Service'
-
-PROJECT_URL = os.environ.get(
-    'PROJECT_URL', 'http://127.0.0.1:8089')
+PROJECT_URL = os.environ['PROJECT_URL']
 
 
 # -- Directories/paths -------------------------------------------------------
@@ -27,14 +26,12 @@ ALEMBIC_CONFIG_PATH = os.path.join(MIGRATIONS_DIR, 'alembic.ini')
 # -- Database ----------------------------------------------------------------
 
 SQL_ALCHEMY_SETTINGS = {
-    'echo': DEBUG and 0,
+    'echo': DEBUG,
     'pool_pre_ping': True,
-    'pool_size': 20,
+    'pool_size': int(os.environ['DATABASE_CONN_POLL_SIZE']),
 }
-SQLITE_PATH = os.path.join(VAR_DIR, 'db.sqlite')
-TEST_SQLITE_PATH = os.path.join(VAR_DIR, 'db.test.sqlite')
-# DATABASE_URI = os.environ.get('DATABASE_URI', 'sqlite:///%s' % SQLITE_PATH)
-DATABASE_URI = os.environ.get('DATABASE_URI', 'postgresql://postgres:1234@172.17.0.2/datahub')
+
+DATABASE_URI = os.environ['DATABASE_URI']
 
 USING_POSTGRES = DATABASE_URI.startswith('postgresql://')
 USING_SQLITE = DATABASE_URI.startswith('sqlite://')
@@ -42,54 +39,37 @@ USING_SQLITE = DATABASE_URI.startswith('sqlite://')
 
 # -- Services ----------------------------------------------------------------
 
-ELOVERBLIK_TOKEN = os.environ.get(
-    'ELOVERBLIK_TOKEN', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlblR5cGUiOiJUSElSRFBBUlRZQVBJX1JlZnJlc2giLCJ0b2tlbmlkIjoiYTk0YTlhMDMtN2ZhMy00YzJjLTljMGItNTkzYTVjMjFjZWFmIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZWlkZW50aWZpZXIiOiJHVUlJbnRyb1RoaXJkUGFydHkwMSIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL2dpdmVubmFtZSI6IkZvcm5hdm4iLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zdXJuYW1lIjoiRWZ0ZXJuYXZuIiwianRpIjoiYTk0YTlhMDMtN2ZhMy00YzJjLTljMGItNTkzYTVjMjFjZWFmIiwiY3ZyIjoiMjcwNzE0MDEiLCJ0cGlkIjoiR1VJSW50cm9UaGlyZFBhcnR5MDFUcElkIiwibG9naW5UeXBlIjoiS2V5Q2FyZCIsImV4cCI6MTYxODM4NjkzMiwiaXNzIjoiRW5lcmdpbmV0IiwidG9rZW5OYW1lIjoidGVzdDQyIiwicm9sZXMiOiJSZWFkUHJpdmF0ZSwgUmVhZEJ1c2luZXNzIiwiYXVkIjoiRW5lcmdpbmV0In0.UUy227mMtCS-tMEo7ROeavuKSMeCP9N9d0YniGSodK8')
+ACCOUNT_SERVICE_URL = os.environ['ACCOUNT_SERVICE_URL']
+LEDGER_URL = os.environ['LEDGER_URL']
+ENERGY_TYPE_SERVICE_URL = os.environ['ENERGY_TYPE_SERVICE_URL']
 
-ELOVERBLIK_THIRD_PARTY_ID = os.environ.get(
-    'ELOVERBLIK_THIRD_PARTY_ID', 'GUIIntroThirdParty01TpId')
-
-ELOVERBLIK_REQUEST_ACCESS_FROM = date.fromisoformat(os.environ.get(
-    'ELOVERBLIK_REQUEST_ACCESS_FROM', '2016-04-23'))
-
-ELOVERBLIK_REQUEST_ACCESS_TO = date.fromisoformat(os.environ.get(
-    'ELOVERBLIK_REQUEST_ACCESS_TO', '2023-04-17'))
-
-ELOVERBLIK_SERVICE_URL = os.environ.get(
-    'ELOVERBLIK_SERVICE_URL', 'https://apipreprod.eloverblik.dk/ThirdPartyApi')
-
-ELOVERBLIK_ONBOARDING_URL = os.environ.get(
-    'ELOVERBLIK_ONBOARDING_URL', 'https://preprod.eloverblik.dk/Authorization/authorization')
-
-ACCOUNT_SERVICE_URL = os.environ.get(
-    'ACCOUNT_SERVICE_URL', 'http://127.0.0.1:8085')
-
-LEDGER_URL = os.environ.get(
-    'LEDGER_URL', 'http://127.0.0.1:8008')
-
-ENERGY_TYPE_SERVICE_URL = os.environ.get(
-    'ENERGY_TYPE_SERVICE_URL', 'http://127.0.0.1:8765')
+ELOVERBLIK_TOKEN = os.environ['ELOVERBLIK_TOKEN']
+ELOVERBLIK_THIRD_PARTY_ID = os.environ['ELOVERBLIK_THIRD_PARTY_ID']
+ELOVERBLIK_SERVICE_URL = os.environ['ELOVERBLIK_SERVICE_URL']
+ELOVERBLIK_ONBOARDING_URL = os.environ['ELOVERBLIK_ONBOARDING_URL']
+ELOVERBLIK_REQUEST_ACCESS_FROM = date.fromisoformat(
+    os.environ['ELOVERBLIK_REQUEST_ACCESS_FROM'])
+ELOVERBLIK_REQUEST_ACCESS_TO = date.fromisoformat(
+    os.environ['ELOVERBLIK_REQUEST_ACCESS_TO'])
 
 
 # -- Auth/tokens -------------------------------------------------------------
 
 TOKEN_HEADER = 'Authorization'
 
-HYDRA_URL = os.environ.get(
-    'HYDRA_URL', 'https://localhost:9100')
-
-HYDRA_INTROSPECT_URL = os.environ.get(
-    'HYDRA_INTROSPECT_URL', 'https://localhost:9101/oauth2/introspect')
+HYDRA_URL = os.environ['HYDRA_URL']
+HYDRA_INTROSPECT_URL = os.environ['HYDRA_INTROSPECT_URL']
 
 
 # -- Task broker and locking -------------------------------------------------
 
-REDIS_HOST = os.environ.get('REDIS_HOST', '172.17.0.3')
-REDIS_PORT = int(os.environ.get('REDIS_PORT', 6379))
-REDIS_USERNAME = os.environ.get('REDIS_USERNAME', '')
-REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD', '')
-REDIS_CACHE_DB = int(os.environ.get('REDIS_CACHE_DB', 6))
-REDIS_BROKER_DB = int(os.environ.get('REDIS_BROKER_DB', 7))
-REDIS_BACKEND_DB = int(os.environ.get('REDIS_BACKEND_DB', 8))
+REDIS_HOST = os.environ['REDIS_HOST']
+REDIS_PORT = int(os.environ['REDIS_PORT'])
+REDIS_USERNAME = os.environ['REDIS_USERNAME']
+REDIS_PASSWORD = os.environ['REDIS_PASSWORD']
+REDIS_CACHE_DB = int(os.environ['REDIS_CACHE_DB'])
+REDIS_BROKER_DB = int(os.environ['REDIS_BROKER_DB'])
+REDIS_BACKEND_DB = int(os.environ['REDIS_BACKEND_DB'])
 
 REDIS_URL = 'redis://%s:%s@%s:%d' % (
     REDIS_USERNAME, REDIS_PASSWORD, REDIS_HOST, REDIS_PORT)
