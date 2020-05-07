312f39f8645f7795be98f7f4e13611d31bab35de
Jakob Kristensen <jakob@jakobk.dk>
2020-05-07 13:31:53 +0200

    Added scheduled job to refresh all Disclosures at 03 AM

--- src/datahub/pipelines/schedule.py
+++ src/datahub/pipelines/schedule.py
@@ -3,6 +3,7 @@ from celery.schedules import crontab
 from datahub.tasks import celery_app
 
 from .import_measurements import get_distinct_gsrn
+from .compile_disclosure import get_disclosures
 
 
 @celery_app.on_after_configure.connect
@@ -12,4 +13,10 @@ def setup_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(
         crontab(hour=3, minute=0),
         get_distinct_gsrn.s(),
+    )
+
+    # Executes every night at 03:00
+    sender.add_periodic_task(
+        crontab(hour=3, minute=0),
+        get_disclosures.s(),
     )
