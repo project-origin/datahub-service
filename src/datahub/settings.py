7b2cde5058a99a44022d38bceb320bc1fea20ca4
Jakob Kristensen <jakob@jakobk.dk>
2020-05-12 14:20:43 +0200

    Added SERVICE_NAME environment variable

--- src/datahub/settings.py
+++ src/datahub/settings.py
@@ -8,6 +8,7 @@ DEBUG = os.environ.get('DEBUG') in ('1', 't', 'true', 'yes')
 # -- Project -----------------------------------------------------------------
 
 PROJECT_NAME = 'DataHub Service'
+SERVICE_NAME = os.environ['SERVICE_NAME']
 SECRET = os.environ['SECRET']
 PROJECT_URL = os.environ['PROJECT_URL']
 CORS_ORIGINS = os.environ['CORS_ORIGINS']
