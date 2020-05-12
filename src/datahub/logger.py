97113f91de089d49cbfd90cf3c56dbdcbdd4e514
Jakob Kristensen <jakob@jakobk.dk>
2020-05-12 12:29:30 +0200

    Added cloud_roleName to Azure exporter

--- src/datahub/logger.py
+++ src/datahub/logger.py
@@ -1,5 +1,8 @@
 import logging
 from functools import partial, wraps
+from opencensus.trace.tracer import Tracer
+from opencensus.trace.samplers import ProbabilitySampler
+from opencensus.ext.azure.trace_exporter import AzureExporter
 from opencensus.ext.azure.log_exporter import AzureLogHandler
 
 from .tasks import Retry
@@ -7,17 +10,30 @@ from .settings import PROJECT_NAME, AZURE_APP_INSIGHTS_CONN_STRING
 
 
 logger = logging.getLogger(PROJECT_NAME)
+handler = None
+exporter = None
+sampler = None
 
 
 if AZURE_APP_INSIGHTS_CONN_STRING:
     print('Exporting logs to Azure Application Insight', flush=True)
 
+    def __telemetry_processor(envelope):
+        envelope.data.baseData.cloud_roleName = PROJECT_NAME
+        envelope.tags['ai.cloud.role'] = PROJECT_NAME
+
     handler = AzureLogHandler(
         connection_string=AZURE_APP_INSIGHTS_CONN_STRING,
         export_interval=5.0,
     )
-
+    handler.add_telemetry_processor(__telemetry_processor)
     logger.addHandler(handler)
+
+    exporter = AzureExporter(connection_string=AZURE_APP_INSIGHTS_CONN_STRING)
+    exporter.add_telemetry_processor(__telemetry_processor)
+
+    sampler = ProbabilitySampler(1.0)
+    tracer = Tracer(exporter=exporter, sampler=sampler)
 
     def __route_extras_to_azure(f, *args, extra=None, **kwargs):
         if extra is None:
@@ -33,6 +49,7 @@ if AZURE_APP_INSIGHTS_CONN_STRING:
     debug = partial(__route_extras_to_azure, logger.debug)
     exception = partial(__route_extras_to_azure, logger.exception)
 else:
+    tracer = Tracer()
     error = logger.error
     critical = logger.critical
     warning = logger.warning
