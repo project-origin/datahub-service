70c688696a009a71f40d4864b79b82ad3e6c7537
Jakob Kristensen <jakob@jakobk.dk>
2020-05-25 11:37:28 +0200

    Changed Import Measurements pipelines to execute every 3rd hour

--- src/datahub/pipelines/schedule.py
+++ src/datahub/pipelines/schedule.py
@@ -9,12 +9,14 @@ from .compile_disclosure import get_disclosures
 @celery_app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
 
-    # Executes every night at 03:00
+    # IMPORT MEASUREMENTS FROM ELOVERBLIK
+    # Executes every 3rd hour (at 03, 06, 09, etc.)
     sender.add_periodic_task(
-        crontab(hour=3, minute=0),
+        crontab(hour='*/3', minute=0),
         get_distinct_gsrn.s(),
     )
 
+    # COMPILE DISCLOSURES
     # Executes every night at 03:00
     sender.add_periodic_task(
         crontab(hour=3, minute=0),
