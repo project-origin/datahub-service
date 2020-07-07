7dc92c3481a981e5fe2ad7cf5eb7dcecc443d8b4
Jakob Kristensen <jakob@jakobk.dk>
2020-07-07 14:53:46 +0200

    Added emissions when issuing GGOs

--- src/datahub/services/energytypes/service.py
+++ src/datahub/services/energytypes/service.py
@@ -1,5 +1,6 @@
 import json
 import requests
+from typing import Dict, Any
 
 from datahub.settings import ENERGY_TYPE_SERVICE_URL, DEBUG
 
@@ -17,7 +18,7 @@ class EnergyTypeServiceError(Exception):
     Raised when invoking EnergyTypeService results
     in a status code != 200
     """
-    def __init__(self, message, status_code, response_body):
+    def __init__(self, message, status_code=None, response_body=None):
         super(EnergyTypeServiceError, self).__init__(message)
         self.status_code = status_code
         self.response_body = response_body
@@ -35,21 +36,24 @@ class EnergyTypeService(object):
     """
     Interface for importing data from EnergyTypeService.
     """
-    def get_energy_type(self, gsrn):
+    def invoke(self, path, query):
         """
-        Returns a tuple of (technology code, fuel code) for a MeteringPoint.
-
-        :param str gsrn:
-        :rtype (str, str):
-        :return: A tuple of (technologyCode, fuelCode)
+        :param str path:
+        :param collections.abc.Mapping[str, str] query:
+        :rtype collections.abc.Mapping[str, str]:
         """
-        url = f'{ENERGY_TYPE_SERVICE_URL}/get-energy-type'
+        url = '%s%s' % (ENERGY_TYPE_SERVICE_URL, path)
+        headers = {
+            'Content-type': 'application/json',
+            'accept': 'application/json',
+        }
 
         try:
             response = requests.get(
                 url=url,
-                params={'gsrn': gsrn},
+                params=query,
                 verify=not DEBUG,
+                headers=headers,
             )
         except:
             raise EnergyTypeServiceConnectionError(
@@ -74,6 +78,19 @@ class EnergyTypeService(object):
                 response_body=str(response.content),
             )
 
+        return response_json
+
+    def get_energy_type(self, gsrn):
+        """
+        Returns a tuple of (technology code, fuel code) for a MeteringPoint.
+
+        :param str gsrn:
+        :rtype (str, str):
+        :return: A tuple of (technologyCode, fuelCode)
+        """
+        query = {'gsrn': gsrn}
+        response_json = self.invoke('/get-energy-type', query)
+
         if response_json.get('success') is not True:
             raise EnergyTypeUnavailable(
                 response_json.get('message', f'Failed to resolve energy type for GSRN {gsrn}'))
@@ -82,3 +99,19 @@ class EnergyTypeService(object):
             response_json['technologyCode'],
             response_json['fuelCode'],
         )
+
+    def get_emissions(self, gsrn):
+        """
+        Returns a dict of emission data for a MeteringPoint.
+
+        :param str gsrn:
+        :rtype: Dict[str, Dict[str, Any]]
+        """
+        query = {'gsrn': gsrn}
+        response_json = self.invoke('/get-emissions', query)
+
+        if response_json.get('success') is True:
+            assert isinstance(response_json['emissions'], dict)
+            return response_json['emissions']
+        else:
+            return {}
