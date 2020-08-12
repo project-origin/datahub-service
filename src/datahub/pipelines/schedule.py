ba123ca58562ceec3c7166b37cae5dd6222aed34
Jakob Kristensen <jakob@jakobk.dk>
2020-08-12 16:18:30 +0200

    Changed importing of measurements to run at 8:00, 12:00, 18:00, and 23:00

--- src/datahub/pipelines/schedule.py
+++ src/datahub/pipelines/schedule.py
@@ -37,7 +37,7 @@ def setup_periodic_tasks(sender, **kwargs):
     # IMPORT MEASUREMENTS FROM ELOVERBLIK
     # Executes every 3rd hour (at 03, 06, 09, etc.)
     sender.add_periodic_task(
-        crontab(hour='2,5,8,11,14,17,20,23', minute=0),
+        crontab(hour='8,12,18,23', minute=0),
         __get_distinct_gsrn.s(),
     )
 
